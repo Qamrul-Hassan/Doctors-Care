d69345c90e73c1e090ce1d9539e6db4f40049aa8
Qamrul-Hassan <sajal700@gmail.com>
2025-10-15 16:08:53 +0600

    srvice-sction added

--- src/js/services.js
+++ src/js/services.js
@@ -1,3 +1,5 @@
+console.log("Services JS loaded");
+
 const serviceContainers = [
     {
         id: 1,
@@ -29,15 +31,25 @@ const serviceContainers = [
         title: "Saúde do Coração",
         description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim.",
     }
+   
 ]
+ console.log("Service Containers: ", serviceContainers);
 
-   function generateServiceHTML() {
+ const servicesContainer = document.getElementById("services-container");
+ console.log("Services Container Element: ", servicesContainer.innerHTML);
+
+    servicesContainer.innerHTML = serviceContainers.map(service => generateServiceHTML(service)).join("");
+ 
+ function generateServiceHTML(service) {
     return `
     <div class="bg-white shadow-md rounded-lg p-6 hover:scale-105 transform transition-all duration-300 overflow-hidden">
-      <h3 class="text-xl font-semibold mb-2 text-secondary-headline">${service.title}</h3>
+    <div class="p-6 text-left mb-2 ">
+    <i class="ri-check-line p-2 border-2 border-primary rounded-full bg-primary-light01 text-primary"></i>
+      <h4 class="text-xl font-semibold mb-2 mt-6 text-secondary-headline">${service.title}</h4>
       <p class="text-gray-600">${service.description}</p>
     </div>
-    <div>
-    <i class="ri-check-line"></i>
-    `
+    
+     
+    
+    </div>`
    };
